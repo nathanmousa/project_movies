a2146b3193780b2c874797013150875b8a62df1b
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-05 21:19:07 +0000

    Going on lunch

--- lib/project/cli.rb
+++ lib/project/cli.rb
@@ -40,41 +40,11 @@ class MovieDB::CLI
       MovieDB::APIService.search_movie(input)
       clear
       header
-      MovieDB::Movies.all.each.with_index(1) do |movie, index|
+      MovieDB::Movies.all.take(5).each.with_index(1) do |movie, index|
         puts "#{index}. #{movie.title}"
       end
       spacer
-      puts "What movie would you like to see more information on?"
-      input = gets.strip.downcase
-      movie = MovieDB::Movies.all[input.to_i - 1]
-      MovieDB::APIService.search_single_movie(movie)
-      clear
-      header
-      
-      title = Artii::Base.new
-      if movie.title.split.size <= 2
-        puts title.asciify("#{movie.title}") + "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
-      else
-        puts "#{movie.title}"
-        puts "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
-      end
-      spacer
-      puts "Rating: %#{movie.vote_average.to_s.delete('.')}"
-      puts "Genre: #{movie.genre_list}"
-      puts "Status: #{movie.status}"
-      spacer
-      puts "Runtime: #{movie.runtime}"
-      puts "Budget: #{currency(movie.budget)}"
-      puts "Revenue: #{currency(movie.revenue)}"
-      puts "Profit: #{currency(movie.revenue - movie.budget)}"
-      spacer
-      puts "Production Companies: #{movie.production_list}"
-      spacer
-      puts "Description: #{movie.overview}"
-      spacer
-      spacer
-      puts "-----------------------------------------------------------------"
-      puts "Type 'exit' to close this app or 'return' to go back to the main menu."
+      puts "What movie would you like to see more information on? Type 'return' to go back to the main menu."
       input = gets.strip.downcase
       
       if input == 'exit'
@@ -82,8 +52,49 @@ class MovieDB::CLI
       elsif input == 'return'
         clear
         menu
+      elsif input == [1..5]
+        movie = MovieDB::Movies.all[input.to_i - 1]
+        MovieDB::APIService.search_single_movie(movie)
+        clear
+        header
+      
+        title = Artii::Base.new
+        if movie.title.split.size <= 2
+          puts title.asciify("#{movie.title}") + "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
+        else
+          puts "#{movie.title}"
+          puts "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
+        end
+        spacer
+        puts "Rating: %#{movie.vote_average.to_s.delete('.')}"
+        puts "Genre: #{movie.genre_list}"
+        puts "Status: #{movie.status}"
+        spacer
+        puts "Runtime: #{movie.runtime}"
+        puts "Budget: #{currency(movie.budget)}"
+        puts "Revenue: #{currency(movie.revenue)}"
+        puts "Profit: #{currency(movie.revenue - movie.budget)}"
+        spacer
+        puts "Production Companies: #{movie.production_list}"
+        spacer
+        puts "Description: #{movie.overview}"
+        spacer
+        spacer
+        puts "-----------------------------------------------------------------"
+        puts "Type 'exit' to close this app or 'return' to go back to the main menu."
+        input = gets.strip.downcase
+      
+        if input == 'exit'
+          close
+        elsif input == 'return'
+          clear
+          menu
+        else
+          puts "Invalid Response. Please try again or type 'exit' to close the program."
+        end
       else
-        puts "Invalid Response. Please try again or type 'exit' to close the program."
+      puts "Invalid Response. Please try again or type 'exit' to close the program."
+      menu #NEED TO RESTRUCTURE AND SEPERATE METHODS
       end
     end
   end
