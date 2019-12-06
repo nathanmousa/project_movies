d2421023e92dce43f4beacd1ea881257a32b4ea2
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-06 00:20:50 +0000

    Refactored CLI

--- lib/project/cli.rb
+++ lib/project/cli.rb
@@ -7,6 +7,7 @@ class MovieDB::CLI
 
   def menu
     input = nil
+    MovieDB::Movies.reset
     
     header
     puts "What would you like to do today? Type 'exit' anytime to close the program."
@@ -34,7 +35,6 @@ class MovieDB::CLI
     while input != 'exit'
       if input == 'return'
         clear
-        resetdb
         menu
       end
       
@@ -47,7 +47,8 @@ class MovieDB::CLI
       end
       
       spacer
-      puts "What movie would you like to see more information on? Type 'return' to go back to the main menu."
+      puts "What movie would you like to see more information on?"
+      puts "Type 'return' to go back to the main menu."
       select_search
     end
     close
@@ -59,55 +60,55 @@ class MovieDB::CLI
     while input != 'exit'
       if input == 'return'
         clear
-        resetdb
         menu
-      elsif input 
-      end
-      
-      input.numeric?
-      movie = MovieDB::Movies.all[input.to_i - 1]
-      MovieDB::APIService.search_single_movie(movie)
-      clear
-      header
-      title = Artii::Base.new
-      if movie.title.split.size <= 2
-        puts title.asciify("#{movie.title}") + "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
-      else
-        puts "#{movie.title}"
-        puts "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
-      end
-      spacer
-      puts "Rating: %#{movie.vote_average.to_s.delete('.')}"
-      puts "Genre: #{movie.genre_list}"
-      puts "Status: #{movie.status}"
-      spacer
-      puts "Runtime: #{movie.runtime}"
-      puts "Budget: #{currency(movie.budget)}"
-      puts "Revenue: #{currency(movie.revenue)}"
-      puts "Profit: #{currency(movie.revenue - movie.budget)}"
-      spacer
-      puts "Production Companies: #{movie.production_list}"
-      spacer
-      puts "Description: #{movie.overview}"
-      spacer
-      puts "-----------------------------------------------------------------"
-      puts "Type 'exit' to close this app or 'return' to go back to the main menu."
-      
-      input = gets.strip.downcase
-      if input == 'exit'
-        close
-      elsif input == 'return'
+      elsif numeric(input)
         clear
-        resetdb
-        menu
+        header
+        movie = MovieDB::Movies.all[input.to_i - 1]
+        MovieDB::APIService.search_single_movie(movie)
+        title = Artii::Base.new
+        
+        if movie.title.split.size <= 2
+          puts title.asciify("#{movie.title}") + "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
+        else
+          puts "#{movie.title}"
+          puts "#{movie.release_date[5..6]}/#{movie.release_date[8..9]}/#{movie.release_date[0..3]}"
+        end
+        
+        spacer
+        puts "Rating: %#{movie.vote_average.to_s.delete('.')}"
+        puts "Genre: #{movie.genre_list}"
+        puts "Status: #{movie.status}"
+        spacer
+        puts "Runtime: #{movie.runtime}"
+        puts "Budget: #{currency(movie.budget)}"
+        puts "Revenue: #{currency(movie.revenue)}"
+        puts "Profit: #{currency(movie.revenue - movie.budget)}"
+        spacer
+        puts "Production Companies: #{movie.production_list}"
+        spacer
+        puts "Description: #{movie.overview}"
+        spacer
+        puts "-----------------------------------------------------------------"
+        puts "Type 'exit' to close this app or 'return' to go back to the main menu."
+        input = gets.strip.downcase
+        
+        while input != 'exit'
+          if input == 'return'
+            clear
+            menu
+          else
+            puts "Invalid Response. Please try again or type 'exit' to close the program."
+            input = gets.strip.downcase
+          end
+        end
+        
       else
         puts "Invalid Response. Please try again or type 'exit' to close the program."
-        
+        input = gets.strip.downcase
       end
-    else
-    puts "Invalid Response. Please try again or type 'exit' to close the program."
-    menu 
     end
+    close
   end
   
   
@@ -145,8 +146,8 @@ class MovieDB::CLI
   "$#{num.to_s.gsub(/\d(?=(...)+$)/, '\0,')}"
   end
   
-  def resetdb
-    MovieDB::Movies.reset
+  def numeric(string)
+    string.scan(/\D/).empty?
   end
   
 end
